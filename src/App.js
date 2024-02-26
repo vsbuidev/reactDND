438b6e4dec4e7cdf933c8c6fabd54f7571d60b60
vsbuidev <vsb.uidev@gmail.com>
2024-02-26 17:14:24 +0530

    readme and license files are added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TaskManager from "./Components/TaskManager";
 const App = () => {
   return (
     <div>
-      <h1>Drag & Drop Management App</h1>
+      <h1>Drag & Drop Management WebApp</h1>
       <TaskManager />
     </div>
   );
